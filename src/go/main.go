9c5ba9e3f4c071d2abe8f18eca537bceb132bc33
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-26 17:59:26 +1000

    made main.go use the static content getter and a new content type finding function

--- src/go/main.go
+++ src/go/main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"context"
+	"errors"
+	"strings"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var index []byte = nil
-var err error = nil
+func GetContentType(fileName string) (string, error) {
+	switch fileName[strings.LastIndex(fileName, "."):] {
+	case ".txt":
+		return "text/plain", nil
+	case ".html":
+		return "text/html", nil
+	case ".css":
+		return "text/css", nil
+	case ".js":
+		return "application/javascript", nil
+	case ".png":
+		return "image/png", nil
+	case ".ico":
+		return "image/vnd.microsoft.icon", nil
+	default:
+		return "", errors.New("Unknown type for " + fileName)
+	}
+}
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
 // AWS Lambda Proxy Request functionality (default behavior)
@@ -17,22 +36,30 @@ type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, evt events.APIGatewayV2HTTPRequest) (Response, error) {
-	if err != nil {
-		return Response{StatusCode: 404}, err
+	if strings.HasPrefix(evt.RawPath, "/api") {
+		switch evt.RawPath {
+		default:
+			return Response{StatusCode: 404}, errors.New("Api not defined: " + evt.RawPath)
+		}
+	} else {
+		content, err := GetStaticContent(evt.RawPath)
+		if err != nil {
+			return Response{StatusCode: 404}, err
+		} else {
+			contentType, err := GetContentType(evt.RawPath)
+			if err != nil {
+				return Response{StatusCode: 406}, err
+			} else {
+				return Response{
+					StatusCode: 200,
+					Headers: map[string]string{
+						"content-type": contentType,
+					},
+					Body: content,
+				}, nil
+			}
+		}
 	}
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            string(index),
-		Headers: map[string]string{
-			"Content-Type":           "text/html",
-			"X-MyCompany-Func-Reply": "world-handler",
-			"requestedPath":          evt.RawPath,
-		},
-	}
-
-	return resp, nil
 }
 
 func main() {
