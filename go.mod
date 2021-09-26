f43bb23dab56b5be2551d9a34e678e624aeea2e3
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-26 18:19:21 +1000

    fixed the go.mod

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/personal/gsuite-tools
 
 go 1.17
 
-require github.com/aws/aws-lambda-go v1.26.0
+require (
+	github.com/aws/aws-lambda-go v1.26.0
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- src/go/staticContent.go
+++ src/go/staticContent.go
@@ -13,30 +13,30 @@ var contentCache map[string]string
 func GetStaticContent(name string) (string, error) {
 	result, cached := contentCache[name]
 	var err error = nil
-	if cached {
+	if !cached {
 		if strings.Contains(name, "..") {
 			return "", errors.New("Path contains ..")
 		}
 		f, error := os.Open(filepath.Join("ui", name))
-	if error != nil {
-		err = error
-	} else {
-		info, error := f.Stat()
 		if error != nil {
 			err = error
 		} else {
-			size := info.Size()
-			var fileContent []byte = make([]byte, size)
-			f.Read(fileContent)
+			info, error := f.Stat()
+			if error != nil {
+				err = error
+			} else {
+				size := info.Size()
+				var fileContent []byte = make([]byte, size)
+				f.Read(fileContent)
 				result = string(fileContent)
 				contentCache[name] = result
-		}
-		f.Close()
 			}
+			f.Close()
+		}
 	}
 	if err != nil {
 		return "", err
-	}else {
+	} else {
 		return result, nil
 	}
 }
